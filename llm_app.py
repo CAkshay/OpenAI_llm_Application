5dd44df6923270e021842b7678aad40ae4aecf0e
Akshay N Choushetti <akshaychoushetti92@gmail.com>
2024-10-06 17:43:14 +0530

    Update llm_app.py

--- llm_app.py
+++ llm_app.py
@@ -1,4 +1,4 @@
-import os
+'''import os
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -6,6 +6,7 @@ os.environ['OPENAI_API_KEY']=os.getenv('OPENAI_API_KEY')
 os.environ['LANGCHAIN_API_KEY']=os.getenv('LANGCHAIN_API_KEY')
 os.environ['LANGCHAIN_TRACING_V2']='true'
 os.environ['LANGCHAIN_PROJECT']='Simple LLM app with OpenAI'
+'''
 
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate 
@@ -25,7 +26,12 @@ chain=prompt|llm_model|output_parser
 
 
 st.title('Simple LLM app with OpenAI')
+api_key=st.text_input('Enter your OpenAI API key',type='password')
 input_text=st.text_input('Enter your question')
 
-if input_text:
+
+if input_text and api_key:
+    openai.api_key=api_key
     st.write(chain.invoke({'question':input_text}))
+else:
+    st.warning('Please enter your OpenAI API key and your question')
